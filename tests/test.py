dc6c9cb8f6e9cbca986bdd0aa5a87f642acdcd0b
Baptiste Fontaine <batifon@yahoo.fr>
2014-03-14 16:53:57 +0100

    fixing build on Python 2.6

--- tests/test.py
+++ tests/test.py
@@ -11,7 +11,7 @@ from os.path import dirname
 if __name__ == '__main__':
     here = dirname(__file__)
     sys.path.insert(0, here+'/..')
-    suite = unittest.defaultTestLoader.discover(here)
+    suite = unittest.TestLoader().discover(here)
     t = unittest.TextTestRunner().run(suite)
     if not t.wasSuccessful():
         sys.exit(1)
