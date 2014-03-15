7501969b89a431ae7b28feb4aa1fab2ccc38e467
Baptiste Fontaine <batifon@yahoo.fr>
2014-03-15 17:10:10 +0100

    Python 3.3 classifier [ci skip]

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
     ],
     entry_points={
         'console_scripts':[
